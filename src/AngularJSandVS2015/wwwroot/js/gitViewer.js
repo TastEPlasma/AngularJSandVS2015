4c4ccbd351a78aa8d929c75edf14f85d975382a8
ER Webster <TastEPlasma@gmail.com>
2015-12-18 09:05:24 -0600

    Tutorial Increment
    Following Scott Allens tutorial @ pluralsight

--- src/AngularJSandVS2015/wwwroot/js/gitViewer.js
+++ src/AngularJSandVS2015/wwwroot/js/gitViewer.js
@@ -1,12 +1,24 @@
 ﻿var viewer = angular.module("githubViewer", []);
 
+//Alternative assignment of controller to module
+//viewer.controller("MainController", function ($scope, $http) {
 
-viewer.controller("MainController", function ($scope, $http) {
+var MainController = function($scope, $http) {
+
     var onUserComplete = function (response) {
         $scope.user = response.data;
     };
 
-    $scope.name = "Nothing";
-});
+    var onError = function (reason) {
+        $scope.error = "Could not fetch the user";
+    };
+
+    $http.get("https://api.github.com/users/angular")
+        .then(onUserComplete, onError);
+
+    $scope.message = "Hello Angular!";
+
+    $scope.name = "Nothign";
+};
 
 viewer.controller("MainController", ["$scope", "$http", MainController]);
