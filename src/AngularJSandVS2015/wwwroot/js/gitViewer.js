56eca3c1af666b47cf870823a5504d58bf5f9a26
ER Webster <TastEPlasma@gmail.com>
2015-12-18 08:51:08 -0600

    Initial Backup
    Environment Set up and tested for learning angularJS

---
+++ src/AngularJSandVS2015/wwwroot/js/gitViewer.js
@@ -0,0 +1,12 @@
+﻿var viewer = angular.module("githubViewer", []);
+
+
+viewer.controller("MainController", function ($scope, $http) {
+    var onUserComplete = function (response) {
+        $scope.user = response.data;
+    };
+
+    $scope.name = "Nothing";
+});
+
+viewer.controller("MainController", ["$scope", "$http", MainController]);
