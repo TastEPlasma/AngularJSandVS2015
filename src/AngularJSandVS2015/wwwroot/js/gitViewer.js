2927d25756a3b88663d94520d213522b6823efe8
ER Webster <TastEPlasma@gmail.com>
2015-12-18 21:36:57 -0600

    Further exploration of directives

--- src/AngularJSandVS2015/wwwroot/js/gitViewer.js
+++ src/AngularJSandVS2015/wwwroot/js/gitViewer.js
@@ -7,10 +7,16 @@ var MainController = function($scope, $http) {
 
     var onUserComplete = function (response) {
         $scope.user = response.data;
+        $http.get($scope.user.repos_url)
+            .then(onRepos, onError);
+    };
+
+    var onRepos = function (response) {
+        $scope.repos = response.data;
     };
 
     var onError = function (reason) {
-        $scope.error = "Could not fetch the user";
+        $scope.error = "Could not fetch the data";
     };
 
     //$http.get("https://api.github.com/users/angular")
@@ -22,6 +28,7 @@ var MainController = function($scope, $http) {
     };
 
     $scope.message = "Github Viewer";
+    $scope.repoSortOrder = "-stargazers_count"
 };
 
 viewer.controller("MainController", ["$scope", "$http", MainController]);
